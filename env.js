77f8604b7ad40f63c75d326b33c20653158746fb
Adrian Kothman <abkothman@gmail.com>
2019-03-27 20:57:12 -0600

    Update database env variable name

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
 module.exports = {
     // https://docs.mongodb.com/manual/reference/connection-string/
-    mongodbConnectionUri: process.env.DATABASE_URL ? process.env.DATABASE_URL : 'mongodb://localhost:27017/reactChatDB'
+    mongodbConnectionUri: process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://localhost:27017/reactChatDB'
 }
