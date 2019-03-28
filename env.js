3f0d96592cf1e68bfd5f39d4ce1b3908216ff106
Adrian Kothman <abkothman@gmail.com>
2019-03-27 20:43:24 -0600

    More heroku config

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
 module.exports = {
     // https://docs.mongodb.com/manual/reference/connection-string/
-    mongodbConnectionUri: 'mongodb://localhost:27017/reactChatDB'
+    mongodbConnectionUri: process.env.DATABASE_URL ? process.env.DATABASE_URL : 'mongodb://localhost:27017/reactChatDB'
 }
