7c97dc6a79663bf8c9e51797478c9122c61b1c73
Adrian Kothman <abkothman@gmail.com>
2019-03-28 20:13:43 -0600

    remove env.js file from repo

--- env.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    // https://docs.mongodb.com/manual/reference/connection-string/
-    mongodbConnectionUri: process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://localhost:27017/reactChatDB'
-}
