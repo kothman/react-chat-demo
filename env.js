385ee6f20301d479f66c7c1839bca8f8e562417e
Adrian Kothman <abkothman@gmail.com>
2019-03-31 00:41:18 -0600

    Fix file formatting

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 module.exports = {
     // https://docs.mongodb.com/manual/reference/connection-string/
-    mongodbConnectionUri: process.env.MONGODB_URI
-    mailgunApiKey: process.env.MAILGUN_API_KEY
+    mongodbConnectionUri: process.env.MONGODB_URI,
+    mailgunApiKey: process.env.MAILGUN_API_KEY,
     mailgunDomain: process.env.MAILGUN_DOMAIN
 }
