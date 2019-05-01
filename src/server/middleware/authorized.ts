dabfc5d9c9fbf91535dff6f4482b16665a58e373
Adrian Kothman <abkothman@gmail.com>
2019-05-01 13:54:20 -0700

    Add some logging

--- src/server/middleware/authorized.ts
+++ src/server/middleware/authorized.ts
@@ -6,11 +6,15 @@ export default function(req: Request, res: Response | any, next: Function) {
     var token = req.session.token || req.headers['x-access-token'];
     if (!token) {
         if (!res.status) return next();
+        console.log('token', token)
         return res.status(401).json({ error: 'Not authorized' });
     }
 
     verify(token, env.secret, (err: Error, decoded: Token) => {
-        if (err && res.status) return res.status(401).send({ error: 'Not authorized' });
+        if (err && res.status) {
+            console.log('error', err);
+            return res.status(401).send({ error: 'Not authorized' });
+        }
         else req.user = decoded;
         return next();
     });    
